5722afd7d6e8a28c0de49b655fbe29261a5aec3b
from10 <lyd0723@naver.com>
2019-12-20 00:22:08 +0900

    Update keras16_return_sequence.py

--- keras16_return_sequence.py
+++ keras16_return_sequence.py
@@ -1,7 +1,7 @@
 from numpy import array
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
-
+ 
 #1.데이터
 x = array([[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6, 7],
             [6, 7, 8], [7, 8, 9], [8, 9, 10], [9, 10, 11], [10, 11, 12],
